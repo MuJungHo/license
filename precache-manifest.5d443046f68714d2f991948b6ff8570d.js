bad418ef38dea1d9064b725ddda96980d95925ad
MuJungHo <ande1218@gmail.com>
2024-06-11 20:51:32 +0800

    Updates

---
+++ precache-manifest.5d443046f68714d2f991948b6ff8570d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8d0ff8d0d780e5683ad30098cd3004",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6e5ed894c407443ebb9",
+    "url": "./static/css/2.378129c6.chunk.css"
+  },
+  {
+    "revision": "7285d16c0254f5282e2d",
+    "url": "./static/css/main.853b41c6.chunk.css"
+  },
+  {
+    "revision": "b6e5ed894c407443ebb9",
+    "url": "./static/js/2.c0dfb6cd.chunk.js"
+  },
+  {
+    "revision": "0023facb0adb4f9ea5db701618575fa8",
+    "url": "./static/js/2.c0dfb6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7285d16c0254f5282e2d",
+    "url": "./static/js/main.a52c7fb1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.a52c7fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c308a55ec5a263fc7ae",
+    "url": "./static/js/runtime-main.090b33ba.js"
+  },
+  {
+    "revision": "daf67dd1ab44163b7a876bf96f19e5b0",
+    "url": "./static/media/delta.daf67dd1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/delta-license/precache-manifest.96477e914fd687f04f0454a16a158cb5.js"
+  "./precache-manifest.5d443046f68714d2f991948b6ff8570d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/delta-license/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
